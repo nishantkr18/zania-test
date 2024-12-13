53122e13d6d10ed497675543ebec3f0817babfdd
nishantkr18 <nniishantkumar@gmail.com>
2024-12-13 22:02:33 +0530

    adding requirements

--- application.py
+++ application.py
@@ -1,3 +1,6 @@
+import sys
+__import__('pysqlite3')
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 from src.get_answers import get_answers
 from src.process_doc import load_data
